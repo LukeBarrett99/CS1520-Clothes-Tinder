a84e1b67a80ca92c86365374877f1afb71256d36
Warfororks <33659948+Warfororks@users.noreply.github.com>
2021-11-01 17:51:47 -0400

    Added backend handlers for sending and receiving data between app and datastore server

--- main.py
+++ main.py
@@ -1,10 +1,51 @@
 import flask
+import user
+import init
 
 app = flask.Flask(__name__)
+us = user.User()
+
+#uncomment these lines to verify that things get added to datastore!
+#us.add_like(init.clothes[0])
+#us.viewed_item()
+#us.viewed_item()
+#us.viewed_item()
+#for l in us.get_liked():
+#    print(l)
 
 @app.route('/')
 def root():
-    return flask.redirect("/static/like_screen.html", code=302)
+    # use render_template to convert the template code to HTML.
+    # this function will look in the templates/ folder for your file.
+    return flask.render_template('homepage.html', page_title='Main Page')
+
+@app.route('/profile_screen.html')
+def profile():
+    return flask.render_template('profile_screen.html')
+
+#for accessing the like screen, all of the user's likes will be stored in an array called likes
+@app.route('/like_screen.html')
+def like():
+    return flask.render_template('like_screen.html', likes=us.get_liked())
+
+#for accessing the homepage, the list of all clothes is in variable clothes and the index is where in the list the page should start showing new clothes
+@app.route('/homepage.html')
+def home():
+    return flask.render_template('homepage.html', clothes=init.clothes, index=us.index)
+
+#point javascript requests to these two listeners!
+@app.route('/liked', methods=['POST','GET'])
+def received_like():
+    clothingIndex = flask.request.form['clothingIndex']
+    us.add_like(init.clothes[clothingIndex])
+    #choose one - either send the index of the clothes or the clothing object itself
+    #clothing = flask.request.form['clothing']
+    #us.add_like(init.clothes[clothing])
+    us.viewed_item()
+
+@app.route('/notliked', methods=['POST','GET'])
+def received_dislike():
+    us.viewed_item()
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
